2dd6dddfd702d0828c7c5d5ff18e720f44d597b1
iamwinner422 <iamwinner422@gmail.com>
2023-04-28 14:16:38 +0000

    Ecriture de la logique

--- popup.js
+++ popup.js
@@ -1,7 +1,29 @@
 document.getElementById('submitButton').addEventListener('click', () => {
     const inputText = document.getElementById('inputText').value;
     const responseArea = document.getElementById('responseArea');
+    let ApiKey = "";
+    chrome.storage.sync.get('OpenAiKey', function (object) {
+        ApiKey = object.OpenAiKey;
+    });
+    if (ApiKey !== "" || typeof(ApiKey) !== "undefined") {
+        if (inputText !== "") {
+            fetch(`https://api.openai.com/v1/engines/davinci/completions`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${ApiKey}`
+                },
+                body: JSON.stringify({
+                    prompt: `Hi ChatGPT! Can you reformulate or resume correctly and simply in the corresponding language this prompt while taking into account the context «${inputText}» ?`,
+                    max_tokens: 1000,
+                    temperature: 0.5,
+                })
+            }).then((response) =>{
+               console.log(response.json());
+            });
 
+        }
+    }
     // Traitez le texte d'entrée et affichez la réponse dans responseArea
     responseArea.textContent = `Your gently question: ${inputText}`;
     responseArea.style.display = "block";
